4ae0578c981b5e0d2bc2d80c23d7a35b107c3141
Fahad Assoumani <fahad.assoumani@epitech.eu>
2025-09-26 22:26:06 +0200

    update: app name, invoke the DDTrace module at startup and add some deps + specify elixirc_paths based on environment

--- mix.exs
+++ mix.exs
@@ -1,19 +1,22 @@
-defmodule DdTraceElixir.MixProject do
+defmodule DdTraceEx.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :dd_trace_elixir,
+      app: :dd_trace_ex,
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {DDTrace, []},
       extra_applications: [:logger]
     ]
   end
@@ -21,8 +24,19 @@ defmodule DdTraceElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:finch, "~> 0.20"},
+      {:ex_doc, "~> 0.34", only: :dev},
+      {:jason, "~> 1.4"},
+      {:mox, "~> 1.0", only: :dev}
     ]
   end
+
+  defp docs do
+    [
+      main: "Datadog"
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_env), do: ["lib"]
 end
