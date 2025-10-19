1c01a0553de7bdb547f70a57d7274ca70e09815f
Fahad Assoumani <fahad.assoumani@epitech.eu>
2025-10-19 22:44:46 +0200

    update(integration_test): format + use TestHelper's function in span_collector_test

--- test/integration/span_collector_test.exs
+++ test/integration/span_collector_test.exs
@@ -3,16 +3,6 @@ defmodule Integration.SpanCollectorTest do
 
   @agent_api_module Application.compile_env!(:dd_trace_ex, :agent_api_module)
 
-  @span %DDTrace.Span{
-    duration: 12345,
-    name: "elixir APM client",
-    resource: "test",
-    service: "Integration test",
-    span_id: 987_654_321,
-    start: 0,
-    trace_id: 123_456_789
-  }
-
   import Mox
 
   setup [:set_mox_from_context, :verify_on_exit!]
@@ -20,9 +10,9 @@ defmodule Integration.SpanCollectorTest do
   test "when the process `flush` and Circuit Breaker is closed, the AgentAPI interface is called" do
     test_pid = self()
     ref = make_ref()
-
     pid = start_supervised!({DDTrace.SpanCollector, [mode: :manual]})
-    DDTrace.SpanCollector.add_span(@span)
+
+    DDTrace.SpanCollector.add_span(TestHelper.single_span())
 
     expect(@agent_api_module, :send_traces, fn _traces ->
       send(test_pid, {:send_traces_called, ref})
@@ -38,23 +28,25 @@ defmodule Integration.SpanCollectorTest do
     test "when the process `flush` retries send_traces until circuit_breaker_threshold" do
       test_pid = self()
       ref = make_ref()
-
       pid = start_supervised!({DDTrace.SpanCollector, [mode: :semi_periodic]})
-      DDTrace.SpanCollector.add_span(@span)
+      span = TestHelper.single_span()
+
+      DDTrace.SpanCollector.add_span(span)
 
       @agent_api_module
       |> expect(:send_traces, fn _traces ->
-        {:error, "unexpected event occurs", [@span.trace_id]}
+        {:error, "unexpected event occurs", [span.trace_id]}
       end)
       |> expect(:send_traces, fn _traces ->
-        {:error, "unexpected event occurs", [@span.trace_id]}
+        {:error, "unexpected event occurs", [span.trace_id]}
       end)
       |> expect(:send_traces, fn _traces ->
-        {:error, "unexpected event occurs", [@span.trace_id]}
+        {:error, "unexpected event occurs", [span.trace_id]}
       end)
 
       send(pid, :flush)
       Process.send_after(pid, {:cb_state, {test_pid, ref}}, 50)
+
       assert_receive({^ref, circuit_breaker})
       assert circuit_breaker.state == :open
     end
@@ -64,32 +56,35 @@ defmodule Integration.SpanCollectorTest do
     test "circuit breaker recover after fails" do
       test_pid = self()
       ref = make_ref()
-
       pid = start_supervised!({DDTrace.SpanCollector, [mode: :manual]})
-      DDTrace.SpanCollector.add_span(@span)
+      span = TestHelper.single_span()
+
+      DDTrace.SpanCollector.add_span(span)
 
       for _ <- 1..3 do
         @agent_api_module
         |> expect(:send_traces, fn _traces ->
-          {:error, "unexpected event occurs", [@span.trace_id]}
+          {:error, "unexpected event occurs", [span.trace_id]}
         end)
 
         send(pid, :flush)
       end
 
       Process.send_after(pid, {:cb_state, {test_pid, ref}}, 90)
+
       assert_receive({^ref, circuit_breaker})
       assert circuit_breaker.state == :open
 
-      DDTrace.SpanCollector.add_span(@span)
+      DDTrace.SpanCollector.add_span(TestHelper.single_span())
 
       @agent_api_module |> expect(:send_traces, fn _traces -> {:ok, :resp} end)
       # Wait for circuit breaker timeout expiration
       t = (System.monotonic_time(:millisecond) - circuit_breaker.next_attempt) |> abs()
+
       Process.send_after(pid, :flush, t)
       Process.send_after(pid, {:cb_state, {test_pid, ref}}, t)
-      assert_receive({^ref, circuit_breaker}, t + 1_000)
 
+      assert_receive({^ref, circuit_breaker}, t + 1_000)
       assert circuit_breaker.state == :closed
     end
   end
@@ -98,30 +93,9 @@ defmodule Integration.SpanCollectorTest do
     test "Span collector tries to resend failed spans" do
       test_pid = self()
       ref = make_ref()
-
-      traces = [
-        @span,
-        %DDTrace.Span{
-          duration: 12345,
-          name: "elixir APM client",
-          resource: "test",
-          service: "Integration test",
-          span_id: 989_654_321,
-          start: 0,
-          trace_id: 125_456_789
-        },
-        %DDTrace.Span{
-          duration: 12345,
-          name: "elixir APM client",
-          resource: "test",
-          service: "Integration test",
-          span_id: 990_654_321,
-          start: 0,
-          trace_id: 127_456_789
-        }
-      ]
-
+      traces = TestHelper.traces()
       pid = start_supervised!({DDTrace.SpanCollector, [mode: :semi_periodic]})
+
       Enum.each(traces, &DDTrace.SpanCollector.add_span/1)
 
       @agent_api_module
@@ -137,6 +111,7 @@ defmodule Integration.SpanCollectorTest do
 
       send(pid, :flush)
       Process.send_after(pid, {:cb_state, {test_pid, ref}}, 50)
+
       assert_receive({^ref, circuit_breaker})
       assert circuit_breaker.state == :closed
     end
